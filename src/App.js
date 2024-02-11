833ac2af50ebdd1533175aa4a005264992ba07a8
Donghwan Oh <imodh97@gmail.com>
2024-02-11 18:20:44 +0900

    #6.3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  useEffect(() => {
+    console.log("I run only once.");
+  }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  };
   return (
     <div>
-      <h1>Welcome Back to React!</h1>
+      <input
+        type="text"
+        placeholder="Search here..."
+        onChange={onChange}
+        value={keyword}
+      ></input>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
