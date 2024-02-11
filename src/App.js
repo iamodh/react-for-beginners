9d0abee8f4e22cfc4b1708e155f916c01c72a360
Donghwan Oh <imodh97@gmail.com>
2024-02-12 00:16:19 +0900

    #6.4

--- src/App.js
+++ src/App.js
@@ -2,32 +2,23 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
+function Hello() {
   useEffect(() => {
-    console.log("I run only once.");
+    console.log("Created :)");
+    return () => {
+      console.log("Destroyed :0");
+    };
   }, []);
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]);
-  const onChange = (event) => {
-    setKeyword(event.target.value);
-  };
+  return <h1>Heello</h1>;
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Search here..."
-        onChange={onChange}
-        value={keyword}
-      ></input>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "HIDE" : "SHOW"}</button>
     </div>
   );
 }
